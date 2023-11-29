bedbe8a682c6ff00e910996b10d4ffc540647fdf
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-11-29 13:31:06 +0000

    add script for MAplot

---
+++ iSEEde/MAPlot.R
@@ -0,0 +1,33 @@
+library("iSEEde")
+library("airway")
+library("DESeq2")
+library("iSEE")
+
+# Example data ----
+
+data("airway")
+airway$dex <- relevel(airway$dex, "untrt")
+rowData(airway)$seq_strand <- factor(rowData(airway)$seq_strand)
+
+dds <- DESeqDataSet(airway, ~ 0 + dex + cell)
+
+dds <- DESeq(dds)
+res_deseq2 <- results(dds, contrast = list("dextrt", "dexuntrt"))
+
+# iSEE / iSEEde ---
+
+airway <- embedContrastResults(res_deseq2, airway, name = "dex: trt vs untrt")
+contrastResults(airway)
+
+app <- iSEE(airway, initial = list(
+  MAPlot(
+    PanelWidth = 12L,
+    ContrastName="dex: trt vs untrt",
+    ColorBy = "Row data",
+    ColorByRowData = "seq_strand"
+  )
+))
+
+if (interactive()) {
+  shiny::runApp(app)
+}
