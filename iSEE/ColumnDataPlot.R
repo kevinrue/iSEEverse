f7f0882b7178b88d86adc4ac6b4fd9cf50beba76
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-10-07 12:06:10 +0100

    fix iSEE/ColumnDataPlot.R to reflect isee.github.io

--- iSEE/ColumnDataPlot.R
+++ iSEE/ColumnDataPlot.R
@@ -20,10 +20,43 @@ sce
 
 app <- iSEE(sce, initial = list(
   ColumnDataPlot(
-    PanelWidth = 8L
+    PanelWidth = 8L,
+    YAxis = "NREADS",
+    XAxis = "Column data",
+    XAxisColumnData = "driver_1_s",
+    ColorBy = "Column data",
+    ColorByColumnData = "driver_1_s",
+    FacetColumnBy = "Column data",
+    FacetColumnByColData = "Core.Type"
     )
 ))
 
 if (interactive()) {
   shiny::runApp(app, port=1234)
 }
+
+
+initial[["ColumnDataPlot1"]] <- new("ColumnDataPlot", XAxis = "Column data", YAxis = "NREADS",
+  XAxisColumnData = "driver_1_s", FacetRowByColData = "driver_1_s",
+  FacetColumnByColData = "Core.Type", ColorByColumnData = "driver_1_s",
+  ColorByFeatureNameAssay = "logcounts", ColorBySampleNameColor = "#FF0000",
+  ShapeByColumnData = "driver_1_s", SizeByColumnData = "NREADS",
+  TooltipColumnData = character(0), FacetRowBy = "None", FacetColumnBy = "Column data",
+  ColorBy = "Column data", ColorByDefaultColor = "#000000",
+  ColorByFeatureName = "0610007P14Rik", ColorByFeatureSource = "---",
+  ColorByFeatureDynamicSource = FALSE, ColorBySampleName = "SRR2140028",
+  ColorBySampleSource = "---", ColorBySampleDynamicSource = FALSE,
+  ShapeBy = "None", SizeBy = "None", SelectionAlpha = 0.1,
+  ZoomData = numeric(0), BrushData = list(), VisualBoxOpen = FALSE,
+  VisualChoices = "Facet", ContourAdd = FALSE, ContourColor = "#0000FF",
+  FixAspectRatio = FALSE, ViolinAdd = TRUE, PointSize = 1,
+  PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+  CustomLabels = FALSE, CustomLabelsText = "SRR2140028", FontSize = 1,
+  LegendPointSize = 1, LegendPosition = "Bottom", HoverInfo = TRUE,
+  LabelCenters = FALSE, LabelCentersBy = "driver_1_s", LabelCentersColor = "#000000",
+  VersionInfo = list(iSEE = structure(list(c(2L, 17L, 4L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(ColumnDataPlot = 1L), PanelHeight = 500L,
+  PanelWidth = 8L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+  ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+  ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+  ColumnSelectionRestrict = FALSE, SelectionHistory = list())
