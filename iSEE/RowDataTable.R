a378c3f8cb5a1fa8967dcf216d8d84dcf6d8ec96
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-10-09 09:16:11 +0100

    simplify code

--- iSEE/RowDataTable.R
+++ iSEE/RowDataTable.R
@@ -1,16 +1,10 @@
 library(iSEE)
-
 library(scRNAseq)
 
 # Example data ----
+
 sce <- ReprocessedAllenData(assays="tophat_counts")
 
-library(scater)
-sce <- logNormCounts(sce, exprs_values="tophat_counts")
-
-sce <- runPCA(sce, ncomponents=4)
-sce <- runTSNE(sce)
-sce <- runUMAP(sce)
 rowData(sce)$ave_count <- rowMeans(assay(sce, "tophat_counts"))
 rowData(sce)$n_cells <- rowSums(assay(sce, "tophat_counts") > 0)
 
--- iSEE/SampleAssayPlot.R
+++ iSEE/SampleAssayPlot.R
@@ -1,27 +1,20 @@
 library(iSEE)
-
 library(scRNAseq)
+library(scater)
 
 # Example data ----
-sce <- ReprocessedAllenData(assays="tophat_counts")
-class(sce)
 
-library(scater)
+sce <- ReprocessedAllenData(assays="tophat_counts")
+
 sce <- logNormCounts(sce, exprs_values="tophat_counts")
 
-sce <- runPCA(sce, ncomponents=4)
-sce <- runTSNE(sce)
-sce <- runUMAP(sce)
-rowData(sce)$ave_count <- rowMeans(assay(sce, "tophat_counts"))
-rowData(sce)$n_cells <- rowSums(assay(sce, "tophat_counts") > 0)
 rowData(sce)$row_var <- rowVars(assay(sce, "logcounts"))
-sce
 
 # launch the app itself ----
 
 app <- iSEE(sce, initial = list(
   SampleAssayPlot(
-    PanelWidth = 8L,
+    PanelWidth = 12L,
     YAxisSampleName = "SRR2140028",
     XAxis = "Sample name", XAxisSampleName = "SRR2140022",
     ColorBy = "Row data", ColorByRowData = "row_var"
@@ -31,4 +24,3 @@ app <- iSEE(sce, initial = list(
 if (interactive()) {
   shiny::runApp(app, port=1234)
 }
-
