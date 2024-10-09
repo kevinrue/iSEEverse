ede53552f5a2dadafc2fd85b477861e48f4bc2c0
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-10-09 09:02:10 +0100

    simplify scripts to essentials

--- iSEE/ReducedDimensionPlot.R
+++ iSEE/ReducedDimensionPlot.R
@@ -1,20 +1,15 @@
 library(iSEE)
-
 library(scRNAseq)
+library(scater)
 
 # Example data ----
-sce <- ReprocessedAllenData(assays="tophat_counts")
-class(sce)
 
-library(scater)
+sce <- ReprocessedAllenData(assays="tophat_counts")
+
 sce <- logNormCounts(sce, exprs_values="tophat_counts")
 
 sce <- runPCA(sce, ncomponents=4)
-sce <- runTSNE(sce)
 sce <- runUMAP(sce)
-rowData(sce)$ave_count <- rowMeans(assay(sce, "tophat_counts"))
-rowData(sce)$n_cells <- rowSums(assay(sce, "tophat_counts") > 0)
-sce
 
 # launch the app itself ----
 
