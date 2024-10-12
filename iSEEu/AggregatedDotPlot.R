aca7ed74d79bec36419c10eec5bed1ce54b14649
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-10-12 16:02:35 +0100

    add script for AggregatedDotPlot

---
+++ iSEEu/AggregatedDotPlot.R
@@ -0,0 +1,26 @@
+library(iSEE)
+library(scRNAseq)
+library(scater)
+library(iSEEu)
+
+# Example data ----
+
+sce <- ReprocessedAllenData(assays="tophat_counts")
+
+sce <- logNormCounts(sce, exprs_values="tophat_counts")
+
+# launch the app itself ----
+
+if (interactive()) {
+  iSEE(
+    sce,
+    initial = list(
+      AggregatedDotPlot(
+        ColumnDataLabel="Primary.Type",
+        CustomRowsText = "Rorb\nSnap25\nFoxp2",
+        # PanelHeight = 500L,
+        PanelWidth = 12L
+      )
+    )
+  )
+}
