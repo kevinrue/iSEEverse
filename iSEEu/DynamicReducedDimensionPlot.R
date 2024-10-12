5082880d907e3714182ae4e396b742b7b427ac8f
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-10-12 17:42:01 +0100

    update code

--- iSEEu/DynamicReducedDimensionPlot.R
+++ iSEEu/DynamicReducedDimensionPlot.R
@@ -3,6 +3,7 @@ library(iSEEu)
 library(scRNAseq)
 library(scater)
 
+set.seed(1)
 sce <- ReprocessedAllenData(assays="tophat_counts")
 sce <- logNormCounts(sce, exprs_values="tophat_counts")
 sce <- runPCA(sce, ncomponents=4)
@@ -10,7 +11,15 @@ sce <- runPCA(sce, ncomponents=4)
 if (interactive()) {
   iSEE(sce, initial=list(
     ReducedDimensionPlot(
-      PanelWidth = 6L
+      PanelWidth = 6L,
+      BrushData = list(
+        lasso = NULL, closed = TRUE,
+        mapping = list(x = "X", y = "Y"),
+        coord = structure(c(
+          -47.8, -41.9, -14.6, -13.6, -19.1, -27.3, -33.6, -44, -47.8,
+          -23.6, -44.1, -56.4, -46.9, -26.4, -17.4, -6.2, -5.4, -23.6),
+          dim = c(9L, 2L))
+      )
     ),
     DynamicReducedDimensionPlot(
       PanelWidth = 6L,
